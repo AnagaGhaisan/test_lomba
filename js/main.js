a7834cb6b5aa2f01f6b5df37f267ba8ce2821575
Aga <anaga.ghaisan@gmail.com>
2024-10-16 09:54:31 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+import Typed from 'typed.js';
+
+const typed = new Typed('#element', {
+  strings: ['Bonjour.', 'Haai.', 'Assalamualaikum.', 'Gday.', 'Hoi.', 'Olá.', 'Ahoj.', 'Ni hao.', 'Hej.', 'Kumusta.', 'Aloha.', 'Namaste.', 'Hello.', 'Dia dhuit.', 'Ciao.', 'Haai.', 'Bonjour.', 'Haai.', 'Bonjour.', 'Haai.', 'Bonjour.', 'Haai.', 'Bonjour.', 'Haai.', 'Bonjour.', 'Haai.', 'Bonjour.', 'Haai.',],
+  typeSpeed: 50,
+});
